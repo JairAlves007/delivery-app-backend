c42b071a94ffdc82366308b3aea5e97360904d81
Jair Alves <ajair2550@gmail.com>
2025-08-04 09:08:35 -0300

    chore: add initial database

--- src/env.ts
+++ src/env.ts
@@ -3,7 +3,8 @@ import z from "zod";
 const envSchema = z.object({
 	PORT: z.string().default("3000"),
 	NODE_ENV: z.enum(["development", "production"]).default("development"),
-	BASE_URL: z.url().default("http://localhost:3000")
+	BASE_URL: z.url().default("http://localhost:3000"),
+	DATABASE_URL: z.url()
 });
 
 export const env = envSchema.parse(process.env);
