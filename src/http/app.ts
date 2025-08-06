4b7a337cf12beb08bae5ec2005fbf21c1f22aa7a
Jair Alves <ajair2550@gmail.com>
2025-08-06 07:58:21 -0300

    feat: validating unknown errors

--- src/http/app.ts
+++ src/http/app.ts
@@ -2,6 +2,7 @@ import { env } from "@/env";
 import { routes } from "@/routes";
 import fastifyCors from "@fastify/cors";
 import fastify from "fastify";
+import { flattenError, ZodError } from "zod";
 
 const app = fastify();
 
@@ -10,5 +11,18 @@ app.register(fastifyCors, {
 });
 
 app.register(routes);
+
+app.setErrorHandler((error, _request, reply) => {
+	if (error instanceof ZodError) {
+		return reply.status(400).send({
+			error: "Validation Error",
+			details: flattenError(error).fieldErrors
+		});
+	}
+
+	if (env.NODE_ENV !== "production") console.error(error);
+
+	return reply.status(500).send({ error: "Internal Server Error" });
+});
 
 export { app };
