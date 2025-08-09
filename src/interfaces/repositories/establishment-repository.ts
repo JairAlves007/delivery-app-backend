d6d4a55f2fde27b09d4287cab6456b5a1b62aeda
Jair Alves <ajair2550@gmail.com>
2025-08-09 19:16:28 -0300

    fix: remove unused import

--- src/interfaces/repositories/establishment-repository.ts
+++ src/interfaces/repositories/establishment-repository.ts
@@ -1,4 +1,4 @@
-import { Establishment, Product } from "@prisma/client";
+import { Establishment } from "@prisma/client";
 import { ICRUDBase } from "../crud-base";
 
 export interface IEstablishmentRepository extends ICRUDBase<Establishment> {}
