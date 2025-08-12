66fc4efceffb9b778a1760e6c2d5d4aab4e78cfc
Jair Alves <ajair2550@gmail.com>
2025-08-11 21:08:58 -0300

    feat: add route not found error handler

--- src/http/app.ts
+++ src/http/app.ts
@@ -1,5 +1,7 @@
 import { env } from "@/env";
 import { routes } from "@/routes";
+import { HTTPStatusCodes } from "@/helpers/http-request-codes";
+import { DefaultErrorResponse } from "@/types/response";
 import fastifyCors from "@fastify/cors";
 import fastify from "fastify";
 import replySendErrorPlugin from "@/plugins/reply-send-error";
@@ -22,6 +24,20 @@ app.setErrorHandler((error, _request, reply) => {
 	if (env.NODE_ENV !== "production") console.error(error);
 
 	return reply.sendError(error);
+});
+
+app.setNotFoundHandler((request, reply) => {
+	const errorResponse: DefaultErrorResponse = {
+		success: false,
+		message: "ROUTE_NOT_FOUND_ERROR",
+		details: {
+			error: {
+				message: `A rota ${request.url} com o protocolo ${request.method} não foi encontrada`
+			}
+		}
+	};
+
+	return reply.status(HTTPStatusCodes.NOT_FOUND).send(errorResponse);
 });
 
 export { app };
