fcfa4544f152296634688238af6d22d610661ec9
Jair Alves <ajair2550@gmail.com>
2026-06-20 16:28:37 -0300

    remove unused deps

--- src/errors/address/not-found-error.ts
+++
@@ -1,8 +0,0 @@
-import { ErrorBase } from "@/errors/error-base.js";
-import { HTTPStatusCodes } from "@/helpers/http-request-codes.js";
-
-export class AddressNotFound extends ErrorBase {
-  constructor() {
-    super("Address not found", HTTPStatusCodes.NOT_FOUND, "ADDRESS_NOT_FOUND");
-  }
-}
--- src/helpers/token.ts
+++
@@ -1,4 +0,0 @@
-import { createHash } from "node:crypto";
-
-export const computeLookupHash = (token: string): string =>
-  createHash("sha256").update(token).digest("hex");
